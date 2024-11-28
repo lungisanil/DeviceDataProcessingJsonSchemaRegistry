c61267908d50c8a737f93cf337a053c438b4aa4a
Lungisani Luvuno <80898579+Llungisani@users.noreply.github.com>
2024-11-28 09:49:56 +0200

    Add workout event process request

---
+++ src/main/java/vowel/solutions/device/data/device/data/event/consumer/WorkoutEventProcessRequest.java
@@ -0,0 +1,87 @@
+package vowel.solutions.device.data.device.data.event.consumer;
+
+public class WorkoutEventProcessRequest {
+    private Long memberId;
+    private String workoutType;
+    private String deviceName;
+    private Float averageSpeed;
+    private Float averageHeartRate;
+    private String workoutId;
+    private Integer processed;
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public WorkoutEventProcessRequest setMemberId(Long memberId) {
+        this.memberId = memberId;
+        return this;
+    }
+
+    public String getWorkoutType() {
+        return workoutType;
+    }
+
+    public WorkoutEventProcessRequest setWorkoutType(String workoutType) {
+        this.workoutType = workoutType;
+        return this;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public WorkoutEventProcessRequest setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+        return this;
+    }
+
+    public Float getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public WorkoutEventProcessRequest setAverageSpeed(Float averageSpeed) {
+        this.averageSpeed = averageSpeed;
+        return this;
+    }
+
+    public Float getAverageHeartRate() {
+        return averageHeartRate;
+    }
+
+    public WorkoutEventProcessRequest setAverageHeartRate(Float averageHeartRate) {
+        this.averageHeartRate = averageHeartRate;
+        return this;
+    }
+
+    public String getWorkoutId() {
+        return workoutId;
+    }
+
+    public WorkoutEventProcessRequest setWorkoutId(String workoutId) {
+        this.workoutId = workoutId;
+        return this;
+    }
+
+    public Integer getProcessed() {
+        return processed;
+    }
+
+    public WorkoutEventProcessRequest setProcessed(Integer processed) {
+        this.processed = processed;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "WorkoutEventProcessRequest{" +
+                "memberId=" + memberId +
+                ", workoutType='" + workoutType + '\'' +
+                ", deviceName='" + deviceName + '\'' +
+                ", averageSpeed=" + averageSpeed +
+                ", averageHeartRate=" + averageHeartRate +
+                ", workoutId='" + workoutId + '\'' +
+                ", processed=" + processed +
+                '}';
+    }
+}
